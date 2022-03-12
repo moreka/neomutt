2fa1f5227036ba0ff9d2292f7b3cb6ee917cd20e
Richard Russon <rich@flatcap.org>
2022-03-12 19:08:43 +0000

    leak

--- test/pattern/leak.c
+++ test/pattern/leak.c
@@ -41,7 +41,7 @@ static void test_one_leak(const char *pattern)
 {
   struct Buffer *err = mutt_buffer_pool_get();
   struct PatternList *p = mutt_pattern_comp(NULL, NULL, pattern, 0, err);
-  mutt_pattern_free(&p);
+  // mutt_pattern_free(&p);
   mutt_buffer_pool_release(&err);
 }
 
