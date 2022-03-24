ab26f74582ca2fefaaa67e3e2c0f4c300b99932c
Pietro Cerutti <gahr@gahr.ch>
2022-03-24 09:57:34 +0000

    Streamline use of mutt_getch_timeout

--- question/question.c
+++ question/question.c
@@ -131,9 +131,7 @@ int mutt_multi_choice(const char *prompt, const char *letters)
 
     mutt_refresh();
     /* SigWinch is not processed unless timeout is set */
-    mutt_getch_timeout(30 * 1000);
-    ch = mutt_getch();
-    mutt_getch_timeout(-1);
+    ch = mutt_getch_timeout(30 * 1000);
     if (ch.ch == OP_TIMEOUT)
       continue;
     /* (ch.ch == 0) is technically possible.  Treat the same as < 0 (abort) */
@@ -284,9 +282,7 @@ enum QuadOption mutt_yesorno(const char *msg, enum QuadOption def)
 
     mutt_refresh();
     /* SigWinch is not processed unless timeout is set */
-    mutt_getch_timeout(30 * 1000);
-    ch = mutt_getch();
-    mutt_getch_timeout(-1);
+    ch = mutt_getch_timeout(30 * 1000);
     if (ch.ch == OP_TIMEOUT)
       continue;
     if (CI_is_return(ch.ch))
